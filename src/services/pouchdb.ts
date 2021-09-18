8a46f095cb848b919c7b54121537da4649b862fc
Pedro Henrique <pedro.lg.cs@gmail.com>
2021-09-17 21:43:44 -0300

    remove adapter

--- src/services/pouchdb.ts
+++ src/services/pouchdb.ts
@@ -1,12 +1,10 @@
 import PouchDB from 'pouchdb';
-import idbAdapter from 'pouchdb-adapter-idb';
 import pouchdbFind from 'pouchdb-find';
 
 // types
 import { UserSchema } from '../interfaces/user';
 
 // plugins
-PouchDB.plugin(idbAdapter);
 PouchDB.plugin(pouchdbFind);
 
 var pouchDB = new PouchDB<UserSchema>('durga', {
